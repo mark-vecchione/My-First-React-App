d2c4d2e66d1b0c532d109276df2420859a99af1b
Mark Vecchione <187446994+mark-vecchione@users.noreply.github.com>
2025-04-05 13:47:45 -0400

    Created Greeting file and connected to main App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Greeting from './components/Greeting'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,9 @@ function App() {
       <h1>Mark's First React App</h1>
       <div className="hello">
           Hello, World!
+      </div>
+      <div>
+        <Greeting />
       </div>
     </>
   )
